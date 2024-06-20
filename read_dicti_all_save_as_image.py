f91b21faa819b013096cb1fc1592c97f9af0fa47
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-06-20 09:13:40 +0200

    Draw tables

--- read_dicti_all_save_as_image.py
+++ read_dicti_all_save_as_image.py
@@ -1,84 +1,23 @@
 from utilities import load_object
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 dicti_all = load_object("dicti_all")
+ord_metric1 = ["GRU_Att_1", "GRU_Att_2", "GRU_Att_3", "GRU_Att_4"]
+ord_metric2 = ["UniTS_longlat_speed_direction", "UniTS_offsets_speed_direction"]
 ord_metric = ["GRU_Att_1", "GRU_Att_2", "GRU_Att_3", "GRU_Att_4", "UniTS_longlat_speed_direction", "UniTS_offsets_speed_direction"]
 hidden_range = [256]
 model_list = ["LSTM", "GRU", "RNN"]
 modes = ["Reference", "Third", "Linear", "Twice"]
+ord_metric3 = []
 for mod_use in modes:
     for model_name in model_list:
             ord_metric.append(model_name + "_" + mod_use + "_256")
+            ord_metric3.append(model_name + "_" + mod_use + "_256")
 metric_dicti = {"NRMSE": 2, "R2": 2, "MAE": 0, "RMSE": 0}
+metric_translate = {"NRMSE": "NRMSE (\%)", "R2": "$R^{2} (\%)$", "MAE": "MAE", "RMSE": "RMSE"}
 list_ws = [2, 3, 4, 5, 10, 20, 30] 
-
-for metric_name_use in list(metric_dicti.keys()):
-    for model_name_use in ord_metric:
-        duplicate_val_all = True
-        duplicate_val = True
-        too_small = True
-        mul_metric = 0
-        rv_metric = 2
-        while too_small or duplicate_val_all:
-            set_values_all = set()
-            set_values = dict()
-            for val_ws in list_ws:
-                set_values[val_ws] = set()
-            max_col = dict()
-            for val_ws in list_ws:
-                max_col[val_ws] = -1000000
-            min_col = dict()
-            for val_ws in list_ws:
-                min_col[val_ws] = 1000000
-            duplicate_val_all = False
-            duplicate_val = False
-            too_small = False
-            str_pr = ""
-            first_line = metric_name_use + " " + model_name_use + " 10^{" + str(mul_metric) + "} " + str(rv_metric)
-            for varname in dicti_all:
-                for val_ws in list_ws:
-                    first_line += " & $" + str(val_ws) + "$s"
-                break
-            for varname in dicti_all:
-                str_pr += varname
-                for val_ws in list_ws:
-                    vv = dicti_all[varname][model_name_use][str(val_ws)][metric_name_use]  
-                    vv = np.round(vv * (10 ** metric_dicti[metric_name_use]) * (10 ** mul_metric), rv_metric)
-                    str_pr += " & $" + str(vv) + "$"
-                    if vv in set_values[val_ws]:
-                        duplicate_val = True
-                    if vv in set_values_all:
-                        duplicate_val_all = True
-                    if "$0." in str_pr:
-                        too_small = True
-                    set_values[val_ws].add(vv)
-                    set_values_all.add(vv)
-                    if vv > max_col[val_ws]:
-                        max_col[val_ws] = vv
-                    if vv < min_col[val_ws]:
-                        min_col[val_ws] = vv
-                str_pr += " \\\\ \\hline\n"
-            if "R2" not in metric_name_use and "NRMSE" not in metric_name_use:
-                if too_small:
-                    mul_metric += 1
-                    rv_metric = 2
-                elif duplicate_val_all:
-                    rv_metric += 1
-            else: 
-                rv_metric += 1
-            if ("R2" in metric_name_use or "NRMSE" in metric_name_use) and (rv_metric > 3 or mul_metric > 3):
-                break
-            if rv_metric > 3 or mul_metric > 6:
-                break
-        if "R2" in metric_name_use:
-            for val_ws in list_ws:
-                str_pr = str_pr.replace("$" + str(max_col[val_ws]) + "$", "$\\mathbf{" + str(max_col[val_ws]) + "}$") 
-        else:
-            for val_ws in list_ws:
-                str_pr = str_pr.replace("$" + str(min_col[val_ws]) + "$", "$\\mathbf{" + str(min_col[val_ws]) + "}$") 
-        #print(first_line + " \\\\ \\hline")
-        #print(str_pr)
 
 metrictouse = ["MAE", "R2"]
 vartouse = ["direction", "speed", "longitude_no_abs", "latitude_no_abs"]
@@ -87,8 +26,7 @@ start_of_table = "\\begin{table*}[!t]\n\t\\begin{center}\n\t\t\\caption{METRICNA
 end_of_table = "\t\t\\end{tabular}}\n\t\\end{center}\n\\end{table*}\n"
 for metric_name_use in metrictouse:
     for varname in vartouse:
-        plt.figure()
-        plt.title(metric_name_use + " " + varname)
+        line_for_model = dict()
         duplicate_val_all = True
         duplicate_val = True
         too_small = True
@@ -122,9 +60,9 @@ for metric_name_use in metrictouse:
                 if "offsets" in model_name_use:
                     continue
                 str_pr += "\t\t\t" + model_name_use.replace("_", " ").replace(" 256", "").replace(" longlat speed direction", "")
-                line_for_model = []
+                line_for_model[model_name_use] = []
                 for val_ws in list_ws: 
-                    line_for_model.append(dicti_all[varname][model_name_use][str(val_ws)][metric_name_use])
+                    line_for_model[model_name_use].append(dicti_all[varname][model_name_use][str(val_ws)][metric_name_use] * (10 ** metric_dicti[metric_name_use]))
                     vv = dicti_all[varname][model_name_use][str(val_ws)][metric_name_use]  
                     vv = np.round(vv * (10 ** metric_dicti[metric_name_use]) * (10 ** mul_metric), rv_metric)
                     str_pr += " & $" + str(vv) + "$"
@@ -140,7 +78,6 @@ for metric_name_use in metrictouse:
                         max_col[val_ws] = vv
                     if vv < min_col[val_ws]:
                         min_col[val_ws] = vv
-                plt.plot(list_ws, line_for_model, label = model_name_use.replace("_", " ").replace(" 256", "").replace(" longlat speed direction", ""))
                 str_pr += " \\\\ \\hline\n"
             if "R2" not in metric_name_use and "NRMSE" not in metric_name_use:
                 if too_small:
@@ -154,8 +91,29 @@ for metric_name_use in metrictouse:
                 break
             if rv_metric > 3 or mul_metric > 6:
                 break
-        plt.legend(ncol = len(ord_metric) / 4)
-        plt.show()
+        if not os.path.isdir("new_img"):
+            os.makedirs("new_img")
+        plt.figure(figsize = (7, 9))
+        plt.subplot(3, 1, 1)
+        plt.title(metric_translate[metric_name_use] + "\n" + translate_varname[varname].capitalize())
+        for model_name_use in ord_metric1:
+            plt.plot(list_ws, line_for_model[model_name_use], label = model_name_use.replace("_", " ").replace(" 256", "").replace(" longlat speed direction", ""))
+        plt.xticks(list_ws)
+        plt.legend(ncol = 2)
+        plt.subplot(3, 1, 2)
+        for model_name_use in ord_metric2:
+            if "offsets" in model_name_use:
+                continue
+            plt.plot(list_ws, line_for_model[model_name_use], label = model_name_use.replace("_", " ").replace(" 256", "").replace(" longlat speed direction", ""))
+        plt.xticks(list_ws)
+        plt.legend()
+        plt.subplot(3, 1, 3)
+        for model_name_use in ord_metric3:
+            plt.plot(list_ws, line_for_model[model_name_use], label = model_name_use.replace("_", " ").replace(" 256", "").replace(" longlat speed direction", ""))
+        plt.xticks(list_ws)
+        plt.legend(ncol = 4, loc = "lower center", bbox_to_anchor = (0.5, -0.5))
+        #plt.show()
+        plt.savefig("new_img/" + metric_name_use + "_" + varname + ".png", bbox_inches = "tight")
         plt.close()
         if "R2" in metric_name_use:
             for val_ws in list_ws:
